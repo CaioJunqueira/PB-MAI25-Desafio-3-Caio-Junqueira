95971f84ff5ef82734cf8c0164f16aaee818abdd
Caio Junqueira <caio.junqueira12@gmail.com>
2025-07-28 16:23:53 -0300

    feat: first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import "@fontsource/inter/400.css"; //regular
+import "@fontsource/inter/500.css"; //medium
+import "@fontsource/inter/600.css"; //semi bold
+import "@fontsource/inter/700.css"; //bold
+
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+
+    <h1>Vite + TypeScript</h1>
+
+
+  </div>
+`
+
